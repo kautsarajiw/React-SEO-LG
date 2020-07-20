fd8b171de9225d8bd2b831a6f16cba2d8ffdfaa3
Kautsar Tubagus Aji <kautsarajiw@gmail.com>
2020-07-20 13:21:19 +0700

    Updates

---
+++ precache-manifest.8ca6bdeb5db4e61d2046f6e7f6da30bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d737ee174ae1dc7d5ee7e6238935f29",
+    "url": "/React-SEO-LG/index.html"
+  },
+  {
+    "revision": "86875f9a375e8ea54d2e",
+    "url": "/React-SEO-LG/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/React-SEO-LG/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-SEO-LG/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86875f9a375e8ea54d2e",
+    "url": "/React-SEO-LG/static/js/main.6dcf2c4f.chunk.js"
+  },
+  {
+    "revision": "a3fe5e0eb5769e005425",
+    "url": "/React-SEO-LG/static/js/runtime-main.22c2162c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-SEO-LG/static/media/logo.5d5d9eef.svg"
+  }
+]);
